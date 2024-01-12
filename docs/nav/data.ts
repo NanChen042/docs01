5e9af37acaf337661b63cd892071c47e427dc049
Southern Wind <2060518839@qq.com>
2024-01-12 18:43:52 +0800

    news

--- docs/nav/data.ts
+++ docs/nav/data.ts
@@ -3,13 +3,13 @@ export const NAV_DATA = [
         title: '效率工具',
         items: [
             {
-                icon: 'assets/index/1405a7c37b382a5818514b2314556b9d_favicon-32x32_v=8944a05a8b601855de116c8a56d3b3ae.png',
+                icon: './assets/index/1405a7c37b382a5818514b2314556b9d_favicon-32x32_v=8944a05a8b601855de116c8a56d3b3ae.png',
                 title: 'TypeScript 演练场',
                 desc: '这是一个给你提供编写，分享和学习 TypeScript 的网站。',
                 link: 'https://www.typescriptlang.org/zh/play'
             },
             {
-            icon: 'assets/index/3d12699f085c767dec60de11fb09d56a_codelf_logo.f4ae25bd.png',
+            icon: './assets/index/3d12699f085c767dec60de11fb09d56a_codelf_logo.f4ae25bd.png',
             title: 'CODELF',
             desc: '不会命名？超级代码命名生成器！(网络不稳定)',
             link: 'https://www.tutieshi.com/'
@@ -29,7 +29,7 @@ export const NAV_DATA = [
     {
         title: 'CSS工具',
         items: [{
-            icon: 'assets/index/image.png',
+            icon: './assets/index/image.png',
             title: 'CSShake',
             desc: '超级抖动库',
             link: 'https://elrumordelaluz.github.io/csshake/'
@@ -41,67 +41,67 @@ export const NAV_DATA = [
             link: 'http://zhongguose.com/#'
         },
         {
-            icon: 'assets/index/9d9885c2d8765fd18491c3248ec7c4a2_apple-touch-icon.png',
+            icon: './assets/index/9d9885c2d8765fd18491c3248ec7c4a2_apple-touch-icon.png',
             title: 'Neumorphism.io',
             desc: '拟物设计 Generate Soft-UI CSS code 设置块级元素的凹凸阴影大小效果',
             link: 'https://neumorphism.io/#'
         },
         {
-            icon: 'assets/index/fec79d3c19cb30c71ceb68c624f97391_favicon5.png',
+            icon: './assets/index/fec79d3c19cb30c71ceb68c624f97391_favicon5.png',
             title: 'ColorSpace',
             desc: '再也不要浪费时间去寻找完美的调色板了！只需输入一种颜色！',
             link: 'https://mycolor.space/'
         },
         {
-            icon: 'assets/index/d01c34c4566c4d611321fa8648764709_apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png',
+            icon: './assets/index/d01c34c4566c4d611321fa8648764709_apple-touch-icon-5ae1a0698dcc2402e9712f7d01ed509a57814f994c660df9f7a952f3060705ee.png',
             title: 'CodePen',
             desc: 'CodePen是一个面向前端设计师和开发人员的社交开发环境。构建和部署一个网站，展示你的工作，构建测试用例来学习和调试，并寻找灵感。',
             link: 'https://codepen.io/'
         },
         {
-            icon: 'assets/index/75f0cfd13eeff99f281ea5eacda9ac90_favicon.png',
+            icon: './assets/index/75f0cfd13eeff99f281ea5eacda9ac90_favicon.png',
             title: 'Glass Morphism',
             desc: '模糊玻璃',
             link: 'https://glassgenerator.netlify.app/#'
         },
         {
-            icon: 'assets/index/ef01529e1e6d2acbffadc36b0af9b27f_logo2.png',
+            icon: './assets/index/ef01529e1e6d2acbffadc36b0af9b27f_logo2.png',
             title: 'CSS Inspiration',
             desc: '可以称之为CSS灵感,里面大部分使用css-doodle实现。 是一个基于 Web-Component 的库。允许我们快速的创建基于 CSS Grid 布局的页面，以实现各种 CSS 效果（或许可以称之为 CSS 艺术）。',
             link: 'https://glassgenerator.netlify.app/#'
         },
         {
-            icon: 'assets/index/25608dc57dfda91dedc4ce36b7ed05de_favicon-fe5a0ff5.png',
+            icon: './assets/index/25608dc57dfda91dedc4ce36b7ed05de_favicon-fe5a0ff5.png',
             title: 'Cool Backgrounds',
             desc: '艺术背景图片',
             link: 'https://coolbackgrounds.io/'
         },
         {
-            icon: 'assets/index/caa6738502d6b652538d5eab7a3728bb_apple-touch-icon.png',
+            icon: './assets/index/caa6738502d6b652538d5eab7a3728bb_apple-touch-icon.png',
             title: 'Open-Source UI',
             desc: '超级小按钮样式！！创建、共享和使用CSS或Tailwind制作的漂亮的自定义元素。',
             link: 'https://uiverse.io/'
         },
         {
-            icon: 'assets/index/image-1.png',
+            icon: './assets/index/image-1.png',
             title: '贝塞尔曲线',
             desc: '贝塞尔曲线，懂得都懂，可以用来尝试购物车小球',
             link: 'https://cubic-bezier.com/#.17,.67,.83,.67'
         },
         {
-            icon: 'assets/index/image-4.png',
+            icon: './assets/index/image-4.png',
             title: 'CSS3自定义滚动条',
             desc: '滚动条大全，查看源代码即可',
             link: 'https://www.xuanfengge.com/demo/201311/scroll/css3-scroll.html'
         },
         {
-            icon: 'assets/index/79d31d89790352ced404850e46b7dd06_apple-touch-icon-60x60.png',
+            icon: './assets/index/79d31d89790352ced404850e46b7dd06_apple-touch-icon-60x60.png',
             title: 'ITMEO',
             desc: '渐变背景大全,一个两个不够，还有更多！！',
             link: 'https://webgradients.com/'
         },
         {
-            icon: 'assets/index/42e45f2a1ce72bbdcadc7aab4353a01e_fav.png',
+            icon: './assets/index/42e45f2a1ce72bbdcadc7aab4353a01e_fav.png',
             title: 'CSS Loaders',
             desc: 'Continuous CSS Loaders集合,进度条加载样式大全',
             link: 'https://webgradients.com/'
