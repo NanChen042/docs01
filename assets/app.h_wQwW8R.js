cd750eb39441dc6f45dee1669b18facd028d42b2
Southern Wind <2060518839@qq.com>
2024-01-13 21:59:42 +0800

    deploy

---
+++ assets/app.h_wQwW8R.js
@@ -0,0 +1,7 @@
+import{aA as o,aQ as p,aR as u,aS as c,aT as l,aU as f,aV as d,aW as m,aX as h,aY as A,aZ as g,aO as v,d as P,aw as w,v as R,j as y,a_ as C,a$ as _,b0 as b,ah as E}from"./chunks/framework.c_X9LrYI.js";import{R as T}from"./chunks/theme.zrRJzCLB.js";function i(e){if(e.extends){const a=i(e.extends);return{...a,...e,async enhanceApp(t){a.enhanceApp&&await a.enhanceApp(t),e.enhanceApp&&await e.enhanceApp(t)}}}return e}const s=i(T),D=P({name:"VitePressApp",setup(){const{site:e,lang:a,dir:t}=w();return R(()=>{y(()=>{document.documentElement.lang=a.value,document.documentElement.dir=t.value})}),e.value.router.prefetchLinks&&C(),_(),b(),s.setup&&s.setup(),()=>E(s.Layout)}});async function O(){const e=j(),a=S();a.provide(u,e);const t=c(e.route);return a.provide(l,t),a.component("Content",f),a.component("ClientOnly",d),Object.defineProperties(a.config.globalProperties,{$frontmatter:{get(){return t.frontmatter.value}},$params:{get(){return t.page.value.params}}}),s.enhanceApp&&await s.enhanceApp({app:a,router:e,siteData:m}),{app:a,router:e,data:t}}function S(){return h(D)}function j(){let e=o,a;return A(t=>{let n=g(t),r=null;return n&&(e&&(a=n),(e||a===n)&&(n=n.replace(/\.js$/,".lean.js")),r=v(()=>import(n),__vite__mapDeps([]))),o&&(e=!1),r},s.NotFound)}o&&O().then(({app:e,router:a,data:t})=>{a.go().then(()=>{p(a.route,t.site),e.mount("#app")})});export{O as createApp};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
